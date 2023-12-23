e9dc4506b03189892ed50b6959573645f638244a
toporovska <116554503+toporovska@users.noreply.github.com>
2023-12-23 18:00:42 +0200

    Update Order.java

--- flowerStore/src/main/java/ua/edu/ucu/apps/flowerStore/Order.java
+++ flowerStore/src/main/java/ua/edu/ucu/apps/flowerStore/Order.java
@@ -1,40 +1,18 @@
 package ua.edu.ucu.apps.flowerStore;
-
+import lombok.*;
 import java.util.LinkedList;
 
-import lombok.ToString;
-import ua.edu.ucu.apps.flowerStore.Delivery;
-import ua.edu.ucu.apps.flowerStore.Payment;
-import ua.edu.ucu.apps.flowerStore.flower;
+import ua.edu.ucu.apps.flowerStore.Delivery.Delivery;
+import ua.edu.ucu.apps.flowerStore.Payment.Payment;
+import ua.edu.ucu.apps.flowerStore.flower.Item;
+
+
 
 @ToString
 public class Order {
     private LinkedList<Item> items = new LinkedList<>();
     private Payment payment;
     private Delivery delivery;
-
-    public void setPaymentStrategy(Payment selectedpayment) {
-        this.payment = selectedpayment;
-    }
-    public void setDeliveryStrategy(Delivery selecteddelivery) {
-        this.delivery = selecteddelivery;
-    }
-    public void calculateTotalPrice() {
-        double totalPrice = items.stream()
-                .mapToDouble(Item::getPrice)
-                .sum();
-        System.out.println("Total price: " + totalPrice);
-    }
-
-    public void processOrder() {
-        System.out.println("Processing the order");
-        payment.pay(getTotalPrice());
-        delivery.deliver(items);
-    }
-    
-    public double getTotalPrice() {
-        return items.stream().mapToDouble(Item::getPrice).sum();
-    }
 
     public void addItem(Item item) {
         items.add(item);
@@ -43,4 +21,20 @@ public class Order {
     public void removeItem(Item item) {
         items.remove(item);
     }
+
+    public double TotalPrice() {
+        double ans = 0;
+        for (Item item : items) {
+            ans += item.getPrice();
+        }
+        return ans;
+    }
+
+    public void processOrder() {
+        System.out.println("Processing the order");
+        payment.pay(TotalPrice());
+        delivery.deliver(items);
+    }
+    
+
 }
